9eb9474d4558df9b0ab5b84b4d37737e03ae7767
Sahidh <sahidhh5@gmail.com>
2024-07-04 15:18:03 +0530

    style added

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -3,6 +3,7 @@ import { FinancialRecordForm } from "./financial-record-form";
 import { FinancialRecordList } from "./financial-record-list";
 import { useFinancialRecords } from "../../contexts/financial-record-context";
 import { useMemo } from "react";
+import "./financial-record.css";
 
 export const Dashboard = () => {
   const { user } = useUser();
